3fe071d7afec89f77ed1cf2200a72e1902de45fb
Alex <119449496+AlexandraPirog@users.noreply.github.com>
2022-12-13 19:51:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum= Variant(num);
+
+int Variant(int num)
+{
+int result =0;
+for (int i = 0; i <num ; i++)
+{
+    result = result + num % 10;
+    num = num/ 10; 
+}
+return result; 
+}
