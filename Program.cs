82947a00b3bcbe8e0cdbef6d7017916344d9b345
Alex <119449496+AlexandraPirog@users.noreply.github.com>
2022-12-13 20:07:33 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
 Console.Write("Введите число N: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int sum= Variant(num);
@@ -7,10 +6,11 @@ int sum= Variant(num);
 int Variant(int num)
 {
 int result =0;
-for (int i = 0; i <num ; i++)
+for (int i = 0; num>0 ; i++)
 {
     result = result + num % 10;
     num = num/ 10; 
 }
 return result; 
 }
+System.Console.WriteLine($"Сумма цифр числа {num} равна {sum}");
